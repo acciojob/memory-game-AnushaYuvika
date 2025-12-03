f5a6755e06df89edb826311162dbc84db8abc887
Anusha Yuvika <yuvikaanusha19@gmail.com>
2025-12-03 17:09:57 +0530

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,52 @@
 
+// import React, { useState } from "react";
+// import './../styles/App.css';
+// import GameBoard from "./GameBoard";
+
+// const App = () => {
+//   const [difficulty, setDifficulty] = useState("");
+//   const [start, setStart] = useState(false);
+
+//   const handleStart = () => {
+//     if (difficulty) setStart(true);
+//     else alert("Please select a difficulty level!");
+//   };
+
+//   return (
+//     <div className="app">
+//       {!start ? (
+//         <div className="levels_container">
+//           <h1>Welcome!</h1>
+//           <h4>Select a difficulty to start the game</h4>
+//           <div>
+//             <input type="radio" name="difficulty" id="easy" value="easy" onChange={(e) => setDifficulty(e.target.value)} />
+//             <label htmlFor="easy">Easy</label>
+//           </div>
+
+//           <div>
+//             <input type="radio" name="difficulty" id="normal" value="normal" onChange={(e) => setDifficulty(e.target.value)} />
+//             <label htmlFor="normal">Normal</label>
+//           </div>
+
+//           <div>
+//             <input type="radio" name="difficulty" id="hard" value="hard" onChange={(e) => setDifficulty(e.target.value)} />
+//             <label htmlFor="hard">Hard</label>
+//           </div>
+
+//           <button style={{ marginTop: "20px" }} onClick={handleStart}>
+//             Start
+//           </button>
+//         </div>
+//       ) : (
+//         <GameBoard difficulty={difficulty} />
+//       )}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 import React, { useState } from "react";
 import './../styles/App.css';
 import GameBoard from "./GameBoard";
@@ -18,19 +66,20 @@ const App = () => {
         <div className="levels_container">
           <h1>Welcome!</h1>
           <h4>Select a difficulty to start the game</h4>
-          <div>
+
+          <div style={{ display: "flex", alignItems: "center", marginTop: "5px" }}>
             <input type="radio" name="difficulty" id="easy" value="easy" onChange={(e) => setDifficulty(e.target.value)} />
-            <label htmlFor="easy">Easy</label>
+            <label htmlFor="easy" style={{ marginLeft: "5px" }}>Easy</label>
           </div>
 
-          <div>
+          <div style={{ display: "flex", alignItems: "center", marginTop: "5px" }}>
             <input type="radio" name="difficulty" id="normal" value="normal" onChange={(e) => setDifficulty(e.target.value)} />
-            <label htmlFor="normal">Normal</label>
+            <label htmlFor="normal" style={{ marginLeft: "5px" }}>Normal</label>
           </div>
 
-          <div>
+          <div style={{ display: "flex", alignItems: "center", marginTop: "5px" }}>
             <input type="radio" name="difficulty" id="hard" value="hard" onChange={(e) => setDifficulty(e.target.value)} />
-            <label htmlFor="hard">Hard</label>
+            <label htmlFor="hard" style={{ marginLeft: "5px" }}>Hard</label>
           </div>
 
           <button style={{ marginTop: "20px" }} onClick={handleStart}>
@@ -44,4 +93,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
