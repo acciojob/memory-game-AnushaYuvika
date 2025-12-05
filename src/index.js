cca48ca5ac859569be2af7c9fb57da3d3ff8e1b8
Anusha Yuvika <yuvikaanusha19@gmail.com>
2025-12-05 14:14:53 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+// src/index.js
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import './App.css';
+import "./index.css"; // या App.css
 
-
-ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
